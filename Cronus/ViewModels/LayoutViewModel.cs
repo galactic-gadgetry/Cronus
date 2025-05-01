e5a21b013f8985e6194216c7cd0a6f2de9239a27
galactic.gadgetry <galactic.gadgetry@gmail.com>
2025-05-01 15:31:56 -0400

    Fix info bar update

--- Cronus/ViewModels/LayoutViewModel.cs
+++ Cronus/ViewModels/LayoutViewModel.cs
@@ -56,6 +56,7 @@ namespace Cronus.ViewModels
                 OnCurrentNavigationBarViewModelChanged;
             _navigationStore.CurrentSideContentViewModelChanged +=
                 OnCurrentSideContentViewModelChanged;
+            _navigationStore.InfoUpdated += OnInfoUpdated;
         }
 
 
